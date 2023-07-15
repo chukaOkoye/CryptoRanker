bb213c0cdf2ab2257a868e23ee1ed8dfb7317a4e
chukaOkoye <chukanorth@hotmail.co.uk>
2023-07-15 13:33:53 +0100

    feat: add courotines

--- app/src/main/java/com/example/coinstask/data/api/ApiService.kt
+++ app/src/main/java/com/example/coinstask/data/api/ApiService.kt
@@ -1,20 +1,24 @@
 package com.example.coinstask.data.api
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
 class ApiService {
-    fun getCoins(): Any? {
-        val url = URL("https://api.coinpaprika.com/v1/coins")
+    private val baseUrl = "https://api.coinpaprika.com/v1"
+
+    suspend fun getCoins(): HttpURLConnection? = withContext(Dispatchers.IO) {
+        val url = URL("$baseUrl/coins")
         val connection = url.openConnection() as? HttpURLConnection
 
-        return try ({
+        return@withContext try {
             connection?.apply {
                 requestMethod = "GET"
-                connectTimeout = 10000
-                readTimeout = 10000
+                connectTimeout = 10000 // Set the connection timeout
+                readTimeout = 10000 // Set the read timeout
 
                 // Get the response from the API
                 val responseCode = responseCode
@@ -31,7 +35,37 @@ class ApiService {
                     null
                 }
             }
-        }).toString() finally {
+        } finally {
+            connection?.disconnect()
+        }
+    }
+
+    suspend fun getCoinDetails(coinId: String): HttpURLConnection? = withContext(Dispatchers.IO) {
+        val url = URL("$baseUrl/coins/$coinId")
+        val connection = url.openConnection() as? HttpURLConnection
+
+        return@withContext try {
+            connection?.apply {
+                requestMethod = "GET"
+                connectTimeout = 10000 // Set the connection timeout
+                readTimeout = 10000 // Set the read timeout
+
+                // Get the response from the API
+                val responseCode = responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val response = StringBuilder()
+                    var line: String?
+                    while (reader.readLine().also { line = it } != null) {
+                        response.append(line)
+                    }
+                    reader.close()
+                    response.toString()
+                } else {
+                    null
+                }
+            }
+        } finally {
             connection?.disconnect()
         }
     }

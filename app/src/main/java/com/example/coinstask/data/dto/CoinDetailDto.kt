0b8bf0234749be66d040bd357b186d84941e8915
chukaOkoye <chuka93@hotmail.co.uk>
2023-07-17 17:38:08 +0100

    refactor: remove commmented dto objects

--- app/src/main/java/com/example/coinstask/data/dto/CoinDetailDto.kt
+++ app/src/main/java/com/example/coinstask/data/dto/CoinDetailDto.kt
@@ -5,16 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class CoinDetailDto(
     @SerializedName ("description") val description: String,
     @SerializedName ("id") val id: String,
-    @SerializedName ("is_active") val is_active: Boolean
-//    val is_new: Boolean,
-//    val links: Links,
-//    val logo: String,
-//    val message: String,
-//    val name: String,
-//    val rank: Int,
-//    val started_at: String,
-//    val symbol: String,
+    @SerializedName ("is_active") val is_active: Boolean,
+    @SerializedName ("name") val name: String,
 //    val tags: List<Tag>,
-//    val type: String,
-//    val whitepaper: Whitepaper
+    @SerializedName ("type") val type: String
 )
